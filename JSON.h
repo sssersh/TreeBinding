87d69f5e3aa18b3b219397942de2862fc35642e2
user <makasy2@rambler.ru>
2021-06-08 19:16:49 +0300

    JSON : add macro for declare array

--- JSON.h
+++ JSON.h
@@ -26,9 +26,17 @@ typedef TreeBinding::NodesNum ItemNum;
  * \warning Each macro call should be placed in different lines
  * \param   ... 1. Child name. 
  * \param   ... 2. Child's data type
- *              2. Required number of childs elements (TreeBinding::NodesNum::MORE_THAN_ONE by default(if this parameter not passed)). 
  */
-#define JSON_CHILD(...) TREE_NODE(__VA_ARGS__)
+#define JSON_CHILD(name, type) TREE_NODE(name, type, 1)
+
+/*!
+ * \brief   JSON array declaration
+ * \warning Each macro call should be placed in different lines
+ * \param   ... 1. Array name. 
+ * \param   ... 2. Array's elements data type
+ *              3. Required number of array elements (TreeBinding::NodesNum::MORE_THAN_ONE by default(if this parameter not passed)). 
+ */
+#define JSON_ARRAY(...) TREE_NODE(__VA_ARGS__)
 
 /*!
  * \brief   JSON element declaration
