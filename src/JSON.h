728721896fcbd13245ea0f4b888e2d03d7532a44
user <makasy2@rambler.ru>
2021-07-28 18:39:49 +0300

    Move sources to src folder

---
+++ src/JSON.h
@@ -0,0 +1,54 @@
+/*! 
+ *  \file JSON.h
+ *  \brief Macro definitions for JSON implementation
+ */
+
+#ifndef _JSON_H_
+#define _JSON_H_
+
+#include "TreeBinding/TreeBinding.h"
+
+namespace JSON
+{
+
+/*!
+ *  \copydoc TreeBinding::Integer
+ */
+typedef TreeBinding::Integer Integer;
+
+/*!
+ *  \copydoc TreeBinding::NodesNum
+ */
+typedef TreeBinding::NodesNum ItemNum;
+
+
+
+/*!
+ * \brief   JSON child declaration
+ * \warning Each macro call should be placed in different lines
+ * \param[in] ... 1. Child name. 
+ * \param[in] ... 2. Child's data type
+ * \param[in] ... 3. Field are optional/mandatory (mandatory by default).
+ */
+#define JSON_CHILD(...) TREE_NODE(__VA_ARGS__)
+
+/*!
+ * \brief   JSON array declaration
+ * \warning Each macro call should be placed in different lines
+ * \param   ... 1. Array name. 
+ * \param   ... 2. Array's elements data type
+ *              3. Required number of array elements (TreeBinding::NodesNum::MORE_THAN_ONE by default(if this parameter not passed)). 
+ */
+#define JSON_ARRAY(...) TREE_NODE(__VA_ARGS__)
+
+/*!
+ * \brief   JSON element declaration
+ * \warning Each macro call should be placed in different lines
+ * \param   dataType Elements's data type
+ */
+#define JSON_ELEMENT(dataType) TREE_TREE(dataType)
+
+} /* namespace JSON */
+
+#endif /* _JSON_H_ */
+  
---
+++ src/Xml.h
@@ -0,0 +1,80 @@
+/*! 
+ *  \file Xml.h
+ *  \brief Macro definitions for XML implementation 
+ */
+
+#ifndef _XML_H_
+#define _XML_H_
+
+#include "TreeBinding/TreeBinding.h"
+
+namespace XML
+{
+
+/*!
+ *  \copydoc TreeBinding::Integer
+ */
+typedef TreeBinding::Integer Integer;
+
+/*!
+ *  \copydoc TreeBinding::NodesNum
+ */
+typedef TreeBinding::NodesNum ItemNum;
+
+/*!
+ * \brief Concatenate 3 string literals
+ * \param[in] 1st literal
+ * \param[in] 2nd literal
+ * \param[in] 3rd literal
+ */
+#define XML_DETAILS_TOKEN_PASTE(x, y, z) x##y##z
+
+/*!
+ * \brief Concatenate 3 string literals
+ * \note  Used this wrapper over XML_DETAILS_TOKEN_PASTE, because it's imposible without it
+ * \param[in] 1st literal
+ * \param[in] 2nd literal
+ * \param[in] 3rd literal
+ */
+#define XML_DETAILS_CONCAT(x,y,z) XML_DETAILS_TOKEN_PASTE(x,y,z)
+
+/*!
+ * \brief XML default path delimeter (string representation)
+ */
+#define XML_DETAILS_PATH_DELIMETER "/"
+
+/*!
+ * \brief XML default path delimeter (char representation)
+ */
+#define XML_PATH_DELIMETER (*(XML_DETAILS_PATH_DELIMETER))
+
+/*!
+ * \brief   XML attribute declaration
+ * \warning Each macro call should be placed in different lines
+ * \param   ... 1. Attribute name. 
+ *              2. Attribute's data type 
+ *              3. Attribute are optional/mandatory (mandatory(TreeBinding::NodesNum::MORE_THAN_ONE) by default(if this parameter not passed)). 
+ *                 If attribute are optional, pass TreeBinding::NodesNum::NOT_SPECIFIED
+ */
+#define XML_ATTR(name, ...) TREE_NODE( XML_DETAILS_CONCAT("<xmlattr>", XML_DETAILS_PATH_DELIMETER, name), __VA_ARGS__)
+
+/*!
+ * \brief   XML child declaration
+ * \warning Each macro call should be placed in different lines
+ * \param   ... 1. Child's data type 
+ *              2. Required number of childs elements (TreeBinding::NodesNum::MORE_THAN_ONE by default(if this parameter not passed)). 
+ */
+#define XML_CHILD_ELEMENTS(...) TREE_NODE("", __VA_ARGS__)
+
+/*!
+ * \brief   XML element declaration
+ * \warning Each macro call should be placed in different lines
+ * \param   ... 1. Element name (in file)
+ *              2. Data type name
+ */
+#define XML_ELEMENT(name, dataType) TREE_TREE(name, dataType)
+
+} /* namespace XML */
+
+#endif /* _XML_H_ */
+  
