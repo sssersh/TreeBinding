1037bfbd2c93e0d3ce0f5cd3a149b405de3e6f56
sssersh <makasy2@rambler.ru>
2021-11-09 22:42:15 +0300

    Add common test

---
+++ tests/src/test_common.cpp
@@ -0,0 +1,27 @@
+/*!
+ * \brief Test XML parsing
+ */
+
+// Test framework
+#include "gtest/gtest.h"
+
+// Tested file
+#include "TreeBinding.h"
+
+
+
+TEST(xml, test_default_element_name) {
+
+    // Define element with default name
+    XML_ELEMENT(XmlElement) {} element;
+
+    const std::string XML_ELEMENT_DATA = R"(<XmlElement></XmlElement>)";
+
+    boost::property_tree::ptree propertyTree;
+    std::istringstream str(XML_ELEMENT_DATA);
+
+    boost::property_tree::read_xml(str, propertyTree);
+
+    ASSERT_NO_THROW(element.parsePtree(propertyTree));
+}
+
