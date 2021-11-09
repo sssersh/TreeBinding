78329e0db30589af10c7e79b46dd3391045b64cf
sssersh <makasy2@rambler.ru>
2021-11-09 14:11:17 +0300

    Add Translator and Exceptions files

---
+++ TreeBinding/Details/Exceptions.h
@@ -0,0 +1,33 @@
+/*!
+ *  \file Exceptions.h
+ *  \brief Tree binding exceptions
+ */
+
+#ifndef _EXCEPTIONS_H
+#define _EXCEPTIONS_H
+
+namespace TreeBinding {
+
+/*!
+ * \brief Exception for signal wrong number of childs elements in tree
+ */
+struct WrongChildsNumException : public std::runtime_error
+{
+    /*!
+     * \brief     WrongChildsNumException constructor
+     * \param[in] nodeName Node name
+     * \param[in] requiredNum Required number of childs nodes
+     * \param[in] actuallyNum Actual number of childs nodes
+     */
+    WrongChildsNumException(std::string const &nodeName,
+                            NodesNum const requiredNum,
+                            NodesNum::ValueType const actuallyNum) :
+            std::runtime_error(
+                    "Invalid number of childs in node " + nodeName + ". Required: " + requiredNum.toString() +
+                    ", present: " + std::to_string(actuallyNum))
+    {}
+};
+
+} // namespace TreeBinding
+
+#endif /* _EXCEPTIONS_H */
---
+++ TreeBinding/Details/Translator.h
@@ -0,0 +1,114 @@
+/*!
+ *  \file  Translator.h
+ *  \brief Translate string value to target type and visa versa
+ */
+
+#ifndef _TREE_BINDING_TRANSLATOR_H_
+#define _TREE_BINDING_TRANSLATOR_H_
+
+#include <type_traits>
+#include <string>
+#include "TreeBindingDecl.h"
+
+namespace TreeBinding
+{
+
+// Overloaded translators declared for arithmetic types and strings
+// Translator functions are templates, to keep it only in header file
+template<typename T>
+struct hasOverloadedTranslator
+{
+    static const bool value = std::is_arithmetic<T>::value || std::is_same<T, std::string>::value;
+};
+
+/*!
+ *  \brief  Translate string value to target type and visa versa
+ */
+struct Translator
+{
+    /*!
+     *  \brief  Translate string value to target type
+     *  \note   Not used direct get<T2> from boost tree, because it's necessary for forward instance of boost translator_between
+     *  \tparam Target type
+     *  \param[in]  str   String representation of value
+     *  \return     value Target value
+     */
+    template<typename T>
+    static std::enable_if_t<!hasOverloadedTranslator<T>::value, T>
+    fromString(std::string const &str);
+
+    /*!
+     *  \brief     Translate target type to string value
+     *  \tparam    Target type
+     *  \param[in] value Target value
+     *  \return    String representation of value
+     */
+    template<typename T>
+    static std::enable_if_t<!hasOverloadedTranslator<T>::value, std::string>
+    toString(const T& value);
+
+    /*!
+     *  \brief     Translate arithmetic to string value
+     *  \param[in] value Arithmetic value
+     *  \return    String representation of value
+     */
+    template<typename T>
+    static std::enable_if_t<std::is_arithmetic<T>::value, std::string>
+    toString(const T& value)
+    {
+        return std::to_string(value);
+    }
+
+    /*!
+     *  \brief     Translate string to string value
+     *  \param[in] value Source value
+     *  \return    Copy of same string
+     */
+    template<typename T>
+    static std::enable_if_t<std::is_same<T, std::string>::value, std::string>
+    toString(const T& value)
+    {
+        return value;
+    }
+
+    /*!
+     *  \brief      Translate string value to integer
+     *  \param[in]  str   String representation of value
+     *  \return     Integer value
+     */
+    template<typename T>
+    static std::enable_if_t<std::is_integral<T>::value, T>
+    fromString(const std::string& str)
+    {
+        return std::stoll(str);
+    }
+
+    /*!
+     *  \brief      Translate string value to floating
+     *  \param[in]  str   String representation of value
+     *  \return     Float value
+     */
+    template<typename T>
+    static std::enable_if_t<std::is_floating_point<T>::value, T>
+    fromString(const std::string& str)
+    {
+        return std::stod(str.c_str());
+    }
+
+    /*!
+     *  \brief     Translate string value to string value
+     *  \param[in] str   Source string
+     *  \return    Same string
+     */
+    template<typename T>
+    static std::enable_if_t<std::is_same<T, std::string>::value, std::string>
+    fromString(const std::string &str)
+    {
+        return str;
+    }
+
+};
+
+} // namespace TreeBinding
+
+#endif /* _TREE_BINDING_TRANSLATOR_H_ */
