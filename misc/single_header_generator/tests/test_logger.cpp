dba247a8e1f021925308da27e3aca17511a267be
sssersh <makasy2@rambler.ru>
2022-09-15 20:08:46 +0300

    Implement all tests for single header generator logger

--- misc/single_header_generator/tests/test_logger.cpp
+++ misc/single_header_generator/tests/test_logger.cpp
@@ -8,9 +8,14 @@
 using namespace one_header_gen;
 namespace filesystem = std::experimental::filesystem;
 
-class LoggerTest : public ::testing::Test
+class logger_test : public ::testing::Test
 {
 public:
+    void SetUp() override
+    {
+        logger_t::set_out_file(logger_test::filename());
+    }
+
     void TearDown() override
     {
         filesystem::remove(filename());
@@ -19,9 +24,46 @@ public:
     static std::string filename() { return "out.log"; }
 };
 
-TEST_F(LoggerTest, CreateFile)
+TEST_F(logger_test, create_file)
 {
-    logger_t::set_out_file(LoggerTest::filename());
+    ASSERT_TRUE(std::experimental::filesystem::exists(logger_test::filename()));
+}
 
-    ASSERT_TRUE(std::experimental::filesystem::exists(LoggerTest::filename()));
+TEST_F(logger_test, write_to_file)
+{    
+    logger_t::write("First string\n");
+    logger_t::write("Second string ", "Argument 2 ", 555);
+
+    logger_t::set_out_file("");
+
+    std::ifstream file {logger_test::filename()};
+    std::vector<std::string> lines(2);
+
+    std::getline(file, lines[0]);
+    std::getline(file, lines[1]);
+
+    ASSERT_EQ(lines[0], "First string");
+    ASSERT_EQ(lines[1], "Second string Argument 2 555");    
+}
+
+static void write_test()
+{
+    LOG("String 1");
+    LOG("String 2 ", "Second argument ", 111);
+}
+
+TEST_F(logger_test, write_to_file_from_foo)
+{    
+    write_test();
+
+    logger_t::set_out_file("");
+
+    std::ifstream file {logger_test::filename()};
+    std::vector<std::string> lines(2);
+
+    std::getline(file, lines[0]);
+    std::getline(file, lines[1]);
+
+    ASSERT_EQ(lines[0], "[       write_test       ] String 1");
+    ASSERT_EQ(lines[1], "[       write_test       ] String 2 Second argument 111");    
 }
